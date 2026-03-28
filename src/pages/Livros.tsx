ae32ae06fa376349a3526c4840d1f1a6e27e8b50
Sebastian Domingues de Souza <sebastiandomingues1@gmail.com>
2026-03-28 15:19:43 -0300

    Adicionando Imagens que faltavam

--- src/pages/Livros.tsx
+++ src/pages/Livros.tsx
@@ -1,6 +1,9 @@
 import LivroCard from "../componentes/LivroCard";
 import Livro_1 from "../Imagens/Livro_1.webp";
 import Livro_2 from "../Imagens/Livro_2.png";
+import Livro_3 from "../Imagens/Livro_3.jpg";
+import Livro_4 from "../Imagens/Livro_4.png";
+import Livro_5 from "../Imagens/Livro_5.webp";
 
 function Livros() {
   return (
@@ -27,21 +30,21 @@ function Livros() {
           />
           <LivroCard
             id={1}
-            imagem={Livro_1}
+            imagem={Livro_3}
             titulo="Livro 3"
             descricao="zzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz 
                           zzzzzzzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzz"
           />
           <LivroCard
             id={1}
-            imagem={Livro_1}
+            imagem={Livro_4}
             titulo="Livro 4"
             descricao="zzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz 
                           zzzzzzzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzz"
           />
           <LivroCard
             id={1}
-            imagem={Livro_1}
+            imagem={Livro_5}
             titulo="Livro 5"
             descricao="zzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzz 
                           zzzzzzzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzz"
